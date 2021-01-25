29452c10e0b24c9ce33dc0419c5e0c014eb997b2
Artem <artiom.ygrumov@gmail.com>
2021-01-25 07:44:38 +0300

    Выложил все файлы включая _src, кроме node_modules

---
+++ _src/js/main.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+
+
+
+// Слайдер на главном экране
+
+$('.header__slider').slick({
+  dots: true,
+  infinite: false,
+  // slideToShow: 1,
+  prevArrow: '.header__slide-arrow-prev',
+  nextArrow: '.header__slide-arrow-next',
+  dotsClass: 'header__slide__dots',
+})
