e2a670208b6a4b12354b9e6cde42bbc14f043826
Artem <artiom.ygrumov@gmail.com>
2021-01-26 23:16:46 +0300

    Сделал раздел "Награды"

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,7 +3,7 @@
 const body = document.querySelector('body');
 
 // Меню
-
+console.log(body)
 const menu = document.querySelector('.nav__menu'),
       menuBody = document.querySelector('.nav__menu__body'),
       burger = document.querySelector('.nav-burger-icon'),
@@ -61,8 +61,12 @@ accordionHeaders.forEach(accordionHeader => {
   }
 });
 
-
-
+// плавный скролл до якорей
+$("body").on('click', '[href*="#"]', function(e){ // при клике на элементы body содержащие в себе "href=#"  =>
+  const fixedOffset = 0; 
+  $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixedOffset }, 1000); // пользователя переведет на указанный в href якорь со скоростью 1000ms и с отступом 100px сверху
+  e.preventDefault();
+});
 
 // Слайдер на главном экране
 
@@ -75,3 +79,27 @@ $('.header__slider').slick({
   nextArrow: '.header__slide-arrow-next',
   dotsClass: 'header__slide__dots',
 });
+
+// Смена картинок в разделе "Награды"
+
+const pointAwards = document.querySelectorAll('.awards__point-link');
+
+for (let pointAward of pointAwards) {
+  pointAward.addEventListener('click', e => {
+    for (let pointAward of pointAwards) {
+      pointAward.classList.remove('_selected');
+    }
+    pointAward.classList.add('_selected');
+    
+    const pointAwardData = pointAward.dataset.img,
+          imgAward = document.querySelector('.awards-img[src="img/' + pointAwardData + '"]'),
+          imgAwards = document.querySelectorAll('.awards-img');
+      
+    for (let i = 0; i < imgAwards.length; i++) {
+      imgAwards[i].closest('.awards__img-item').classList.remove('_show');
+    }
+    imgAward.closest('.awards__img-item').classList.add('_show');
+    console.log(imgAward);
+    e.preventDefault();
+  });
+}
